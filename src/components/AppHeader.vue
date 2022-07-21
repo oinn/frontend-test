7511866bb54769ba359f4168362f0d3fb3939ca0
Vladimir Zaguliaev <oinnwork@outlook.com>
2022-07-21 17:45:57 +0300

    Поправлен компонент Header согласно новому state

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h3>Товаров в корзине на: {{ cartPriceWithCur }}</h3>
+  </header>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'AppHeader', // Переименовал бы в AppHeader
+  computed: {
+    ...mapGetters(['cartPriceWithCur']),
+  },
+};
+</script>
+
+<style lang="scss">
+header {
+  padding: 10px;
+  background: #e5e5e5;
+}
+</style>
