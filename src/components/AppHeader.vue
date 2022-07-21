a479d3285cc8a9612179782e18d316f65892b66c
Vladimir Zaguliaev <oinnwork@outlook.com>
2022-07-21 19:42:06 +0300

    В AppHeader добавлена кнопка очистки корзины

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,24 @@
 <template>
   <header>
     <h3>Товаров в корзине на: {{ cartPriceWithCur }}</h3>
+    <button :disabled="isCartEmpty" @click="clearCart">Очистить</button>
   </header>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapState, mapGetters, mapMutations } from 'vuex';
 
 export default {
   name: 'AppHeader', // Переименовал бы в AppHeader
   computed: {
+    ...mapState(['cart']),
     ...mapGetters(['cartPriceWithCur']),
+    isCartEmpty() {
+      return this.cart.length === 0;
+    },
+  },
+  methods: {
+    ...mapMutations(['clearCart']),
   },
 };
 </script>
@@ -18,6 +26,11 @@ export default {
 <style lang="scss">
 header {
   padding: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+
   background: #e5e5e5;
 }
 </style>
