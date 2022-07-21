59f2749b8f649d954a178bda3750087c2e2cdd85
Vladimir Zaguliaev <oinnwork@outlook.com>
2022-07-21 15:47:08 +0300

    Отрефакторен файл /api/products.js

---
+++ api/mocs.js
@@ -0,0 +1,20 @@
+export const PRODUCTS = [
+  {
+    id: 'potato',
+    title: 'Картошка',
+    price: 49.99,
+    image: '/products/potato.jpg',
+  },
+  {
+    id: 'carrot',
+    title: 'Морковка',
+    price: 55.00,
+    image: '/products/carrot.jpg',
+  },
+  {
+    id: 'cabbage',
+    title: 'Капуста',
+    price: 28.50,
+    image: '/products/cabbage.jpg',
+  }
+];
--- api/products.js
+++ api/products.js
@@ -1,38 +1,16 @@
-const SLEEP_TIME = 200; // Поставил бы пробел после этой строки дабы логически отделить константы от функций
+import { PRODUCTS } from './mocs';
+
+const SLEEP_TIME = 200;
+
 function sleep(fn) {
   return new Promise(resolve => {
-    setTimeout(() => { // можно сократить запись до SetTimeout(() => resolve(fn()), SLEEP_TIME);
-      resolve(fn());
-    }, SLEEP_TIME);
+    setTimeout(() => resolve(fn()), SLEEP_TIME);
   });
 }
 
 const dataService = {
   getProductsList() {
-    // 1. Это констанста (const а не let).
-    // 2. Она лишняя. Можно сразу возвращать массив.
-    // 3. Выделил бы массив (products) в в отдельный файл с моками (/api/mocks.js).
-    let products = [
-      {
-        id: 'potato',
-        title: 'Картошка',
-        price: 49.99,
-        image: '/products/potato.jpg',
-      },
-      {
-        id: 'carrot',
-        title: 'Морковка',
-        price: 55.00,
-        image: '/products/carrot.jpg',
-      },
-      {
-        id: 'cabbage',
-        title: 'Капуста',
-        price: 28.50,
-        image: '/products/cabbage.jpg',
-      }
-    ];
-    return products;
+    return PRODUCTS;
   },
 };
 
@@ -40,5 +18,6 @@ const productsApi = {
   getProductsList() {
     return sleep(dataService.getProductsList);
   },
-}; // Не хватает пробела перед экспортом
+};
+
 export default productsApi;
