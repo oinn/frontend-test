bd084e7ec9dba6b1952d5dee969b574981c7abf6
Vladimir Zaguliaev <oinnwork@outlook.com>
2022-07-21 18:25:03 +0300

    Поправлены пути к изображениям товаров в моках

--- api/mocs.js
+++ api/mocs.js
@@ -4,18 +4,18 @@ export const PRODUCTS = [
     id: 'potato',
     title: 'Картошка',
     price: 49.99,
-    image: '/products/potato.jpg',
+    image: '/images/products/potato.jpg',
   },
   {
     id: 'carrot',
     title: 'Морковка',
     price: 55.00,
-    image: '/products/carrot.jpg',
+    image: '/images/products/carrot.jpg',
   },
   {
     id: 'cabbage',
     title: 'Капуста',
     price: 28.50,
-    image: '/products/cabbage.jpg',
+    image: '/images/products/cabbage.jpg',
   },
 ];
